6fbcbb3db69085842498c8b1211485a1c2a1086f
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-10-25 19:37:39 +0200

    update: hero | import additional components

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,4 +1,7 @@
 import React from "react"
+import HeroHeading from "./heroHeading"
+import HeroOverlay from "./heroOverlay"
+import InnerSection from "./innerSection"
 
 const Hero = ({ content }) => {
     const { frontmatter } = content;
