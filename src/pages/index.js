637742118a34dedd3391c2339eb1b5c5015afa5a
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-10-25 19:42:07 +0200

    update: import additional components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,52 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
 import Section from "../components/section"
+import InfoBlock from "../components/infoBlock"
+import InfoGrid from "../components/infoGrid"
+import InnerSection from "../components/innerSection"
+import ImageCollage from "../components/imageCollage"
 
 const IndexPage = ({ data }) => {
+	const mainObject = data.hero.edges[0].node
+	const brief = data.brief.edges[1].node
+
+	const briefImages = [
+		mainObject.frontmatter.briefImageOne,
+		mainObject.frontmatter.briefImageTwo,
+		mainObject.frontmatter.briefImageThree
+	]
+
+	const companiesImages = [
+		mainObject.frontmatter.companiesImageOne,
+		mainObject.frontmatter.companiesImageTwo,
+		mainObject.frontmatter.companiesImageThree
+	]
+
 	return (
-		<Layout content={data.hero.edges[0].node}>
-			<Hero content={data.hero.edges[0].node} class="steve" />
-			<Section content={data.hero.edges[0].node} heading="THE BRIEF" gradientType="bottom" dustOverlay="one" />
-			<Section content={data.hero.edges[0].node} heading="COMPANIES & PROJECTS" gradientType="middle" dustOverlay="two" />
-			<Section content={data.hero.edges[0].node} heading="OVERVIEW" gradientType="middle" dustOverlay="three" />
+		<Layout content={mainObject}>
+			<Hero content={mainObject} class="steve" />
+			<Section content={mainObject} heading="THE BRIEF" gradientType="bottom" dustOverlay="one">
+				<InnerSection>
+					<InfoGrid>
+						<InfoBlock heading={ false }>
+							{ brief.frontmatter.introLine }<span role="img" aria-label="emoji">👋🏾</span>
+							<br /> <br />
+							{ brief.frontmatter.paragraphOne }
+							<br /> <br />
+							{ brief.frontmatter.paragraphTwo }
+							<br /> <br />
+							{ brief.frontmatter.endNote }<span role="img" aria-label="emoji">✌🏾</span>
+						</InfoBlock>
+						<ImageCollage imageArray={ briefImages } />
+					</InfoGrid>
+				</InnerSection>
+			</Section>
+			<Section content={mainObject} heading="COMPANIES &amp; PROJECTS" gradientType="middle" dustOverlay="two">
+				Boobs
+			</Section>
+			<Section content={mainObject} heading="OVERVIEW" gradientType="middle" dustOverlay="three">
+				Boobs
+			</Section>
 		</Layout>
 	)
 }
@@ -21,20 +59,20 @@ export default IndexPage
 export const mainQuery = graphql`
 	{
 		hero: allMarkdownRemark {
-		edges {
-			node {
-				frontmatter {
+			edges {
+				node {
+					frontmatter {
 						heroTextOne
 						heroTextTwo
 						heroTextThree
-					backgroundVideo
-					portraitOverlay
-					dustOverlayOne
-					dustOverlayTwo
-					dustOverlayThree
-					instagramIcon
-					githubIcon
-					linkedinIcon
+						backgroundVideo
+						portraitOverlay
+						dustOverlayOne
+						dustOverlayTwo
+						dustOverlayThree
+						instagramIcon
+						githubIcon
+						linkedinIcon
 						briefImageOne
 						briefImageTwo
 						briefImageThree
@@ -53,9 +91,9 @@ export const mainQuery = graphql`
 						paragraphOne
 						paragraphTwo
 						endNote
+					}
 				}
 			}
-		}
 		}
 	}
 `
