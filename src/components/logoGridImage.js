237f5ca04cef02d59a9c2d3f45f24eecaba27247
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-10-26 16:40:42 +0200

    update: apply link to image

--- src/components/logoGridImage.js
+++ src/components/logoGridImage.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const LogoGridImage = ({ imageURL, companyURL }) => {
     return (
-        <a href={ companyURL } target="_blank" rel="noreferrer">
-            <img className="h-24 md:h-36 border-4 border-white mx-auto" src={ imageURL } alt="company-logo" />
+        <a href={ companyURL } title="WeMakeArt" target="_blank" rel="noreferrer">
+            <img className="h-24 md:h-32 lg:h-36 border-4 border-white mx-auto" src={ imageURL } alt="company-logo" />
         </a>
     )
 }
