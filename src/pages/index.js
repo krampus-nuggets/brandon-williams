1ee2c41b3d1134b66f099b71d5432b954180181f
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-10-26 15:44:28 +0200

    update: apply content projects section

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,49 @@ import InfoBlock from "../components/infoBlock"
 import InfoGrid from "../components/infoGrid"
 import InnerSection from "../components/innerSection"
 import ImageCollage from "../components/imageCollage"
+import ProjectGrid from "../components/projectGrid"
+import LogoGrid from "../components/logoGrid"
 
 const IndexPage = ({ data }) => {
-	const mainObject = data.hero.edges[0].node
-	const brief = data.brief.edges[1].node
+	const mainObject = data.main.edges[0].node
+	const briefObject = data.brief.edges[1].node
+	const companyLogosObject = data.companyLogos.edges[2].node
+	const companyURLsObject = data.companyURLs.edges[2].node
 
-	const briefImages = [
+	const briefArtImages = [
 		mainObject.frontmatter.briefImageOne,
 		mainObject.frontmatter.briefImageTwo,
 		mainObject.frontmatter.briefImageThree
 	]
 
-	const companiesImages = [
+	const companyArtImages = [
 		mainObject.frontmatter.companiesImageOne,
 		mainObject.frontmatter.companiesImageTwo,
 		mainObject.frontmatter.companiesImageThree
+	]
+
+	const companyLogos = [
+		companyLogosObject.frontmatter.clover,
+		companyLogosObject.frontmatter.allanGray,
+		companyLogosObject.frontmatter.coronation,
+		companyLogosObject.frontmatter.aiimAfrica,
+		companyLogosObject.frontmatter.eurochem,
+		companyLogosObject.frontmatter.costain,
+		companyLogosObject.frontmatter.pimpMyBook,
+		companyLogosObject.frontmatter.askthuto,
+		companyLogosObject.frontmatter.bluegrassDigital
+	]
+
+	const companyURLs = [
+		companyURLsObject.frontmatter.cloverURL,
+		companyURLsObject.frontmatter.allanGrayURL,
+		companyURLsObject.frontmatter.coronationURL,
+		companyURLsObject.frontmatter.aiimAfricaURL,
+		companyURLsObject.frontmatter.eurochemURL,
+		companyURLsObject.frontmatter.costainURL,
+		companyURLsObject.frontmatter.pimpMyBookURL,
+		companyURLsObject.frontmatter.askthutoURL,
+		companyURLsObject.frontmatter.bluegrassDigitalURL
 	]
 
 	return (
@@ -32,20 +60,25 @@ const IndexPage = ({ data }) => {
 				<InnerSection>
 					<InfoGrid>
 						<InfoBlock heading={ false }>
-							{ brief.frontmatter.introLine }<span role="img" aria-label="emoji">👋🏾</span>
+							{ briefObject.frontmatter.introLine }<span role="img" aria-label="emoji">👋🏾</span>
 							<br /> <br />
-							{ brief.frontmatter.paragraphOne }
+							{ briefObject.frontmatter.paragraphOne }
 							<br /> <br />
-							{ brief.frontmatter.paragraphTwo }
+							{ briefObject.frontmatter.paragraphTwo }
 							<br /> <br />
-							{ brief.frontmatter.endNote }<span role="img" aria-label="emoji">✌🏾</span>
+							{ briefObject.frontmatter.endNote }<span role="img" aria-label="emoji">✌🏾</span>
 						</InfoBlock>
-						<ImageCollage imageArray={ briefImages } />
+						<ImageCollage imageArray={ briefArtImages } />
 					</InfoGrid>
 				</InnerSection>
 			</Section>
 			<Section content={mainObject} heading="COMPANIES &amp; PROJECTS" gradientType="middle" dustOverlay="two">
-				Boobs
+				<InnerSection>
+					<ProjectGrid>
+						<LogoGrid imageArray={ companyLogos } companyURLArray={ companyURLs } />
+						<ImageCollage imageArray={ companyArtImages } />
+					</ProjectGrid>
+				</InnerSection>
 			</Section>
 			<Section content={mainObject} heading="OVERVIEW" gradientType="middle" dustOverlay="three">
 				Boobs
@@ -58,7 +91,7 @@ export default IndexPage
 
 export const mainQuery = graphql`
 	{
-		hero: allMarkdownRemark {
+		main: allMarkdownRemark {
 			edges {
 				node {
 					frontmatter {
@@ -91,6 +124,40 @@ export const mainQuery = graphql`
 						paragraphOne
 						paragraphTwo
 						endNote
+					}
+				}
+			}
+		}
+		companyLogos: allMarkdownRemark {
+			edges {
+				node {
+					frontmatter {
+						pimpMyBook
+						askthuto
+						bluegrassDigital
+						clover
+						allanGray
+						coronation
+						aiimAfrica
+						eurochem
+						costain
+					}
+				}
+			}
+		}
+		companyURLs: allMarkdownRemark {
+			edges {
+				node {
+					frontmatter {
+						pimpMyBookURL
+						askthutoURL
+						bluegrassDigitalURL
+						cloverURL
+						allanGrayURL
+						coronationURL
+						aiimAfricaURL
+						eurochemURL
+						costainURL
 					}
 				}
 			}
