b1621829df081d3dd71cac46a72bbc59d94246eb
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-10-26 15:43:57 +0200

    add: projectGrid component

---
+++ src/components/projectGrid.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const ProjectGrid = ({ children }) => {
+    return (
+        <div className="pt-10 h-4/5 grid grid-cols-1 lg:grid-cols-2 grid-rows-2 lg:grid-rows-1">
+            <div className="w-full mx-auto">
+                { children[0] }
+            </div>
+            <div className="w-full mx-auto">
+                { children[1] }
+            </div>
+        </div>
+    )
+}
+
+export default ProjectGrid
