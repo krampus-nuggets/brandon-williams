c2f38efaa24ac01e37b472fa75d7324ee46c44ed
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-04 22:26:24 +0200

    update: rough flex option added

--- src/components/innerSection.js
+++ src/components/innerSection.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-const InnerSection = ({ children }) => {
-    return (
-        <div className="h-full inner-section relative mx-auto">
-            { children }
-        </div>
-    )
+const InnerSection = ({ children, sectionType }) => {
+    if (sectionType === "flex") {
+        return (
+            <div className="flex flex-col relative mx-auto my-0 p-0 ">
+                { children }
+            </div>
+        )
+    } else {
+        return (
+            <div className="h-full relative mx-auto my-0 p-0 ">
+                { children }
+            </div>
+        )
+    }
+    
 }
 
 export default InnerSection
