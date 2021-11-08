a03410b3b5b312a06f1535e8b2eeee622ea5db44
Brandon Williams <sp3cter.productions@wemakeart.co.za>
2021-11-08 17:18:41 +0200

    update: convert grid to flex

--- src/components/sections/overviewGrid.js
+++ src/components/sections/overviewGrid.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const OverviewGrid = ({ children }) => {
     return (
-        <div className="pt-14 h-4/5 grid grid-cols-1 lg:grid-cols-3 grid-rows-3 lg:grid-rows-1 gap-y-8 lg:gap-y-0">
+        <div className="pt-14 h-4/5 flex flex-col xl:flex-row gap-y-8">
             <div className="w-full mx-auto">
                 { children[0] }
             </div>
