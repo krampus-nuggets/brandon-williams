a260b5353ab6e36bfe07cda426d6b5a583252ea0
Brandon Williams <brandon@wemakeart.co.za>
2023-01-26 23:37:10 +0200

    update: host & formatting clean-up

--- robots-txt.config.js
+++ robots-txt.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    host: "https://brandon-williams.netlify.app",
+    host: "https://brandonwma.com",
     sitemap: "https://res.cloudinary.com/wemakeart/raw/upload/v1636365735/github/brandon-williams/text/sitemap_nqx8q7.xml",
-    policy: [{ userAgent: '*', allow: '/' }]
+    policy: [{ userAgent: "*", allow: "/" }]
 };
